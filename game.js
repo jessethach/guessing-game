e872b15e90b9d8acc6e11254c422cf6e1252960d
jessethach <jessethach@gmail.com>
2015-09-30 13:35:28 -0700

    Added conditions and alerts based on the user response. Users will receive a personalized message depending on whether they answered a question correctly or not.

--- game.js
+++ game.js
@@ -1,8 +1,28 @@
 
 
 var question1 = prompt('Is Jesse from Seattle?'); 
+
+if (question1 === 'Yes') {
+	alert("Correct! Jesse was born and raised in Seattle.");
+} else {
+	alert("Sorry, incorrect. Jesse is and always will be a Seattleite.");
+}
+
 var question2 = prompt('Can Jesse drive a tractor?');
+
+if (question2 === 'Yes') {
+	alert("Wrong! Jesse would be able to CRASH a tractor most likely.");
+} else {
+	alert("You are right! Jesse would most likely crash a tractor.");
+}
+
 var question3 = prompt('Does Jesse Like soccer?');
+
+if (question3 === 'Yes') {
+	alert("Right on! Yes, Jesse loves soccer.");
+} else {
+	alert("You have never been more wrong in your life. Jesse loves soccer.");
+}
 
 console.log('Question 1 response was ' + question1);
 console.log('Question 2 response was ' + question2);
@@ -12,4 +32,3 @@ alert('Your answer to Question 1 was ' + question1);
 alert('Your answer to Question 2 was ' + question2);
 alert('Your answer to Question 3 was ' + question3);
 
-// here are some changes....
