071e0a6b2c46722b62204aebc6881e77010cc09c
jessethach <jessethach@gmail.com>
2015-09-30 15:00:44 -0700

    Included && cases for user yes or no responses. Answers will be accepted reardless of case sensitivity.

--- game.js
+++ game.js
@@ -2,31 +2,37 @@
 
 var question1 = prompt('Is Jesse from Seattle?'); 
 
-if (question1 === 'Yes') {
-	alert("Correct! Jesse was born and raised in Seattle.");
+
+if (question1.toUpperCase() === 'YES' || question1.toUpperCase() === 'Y') {
+	alert('Correct! Jesse was born and raised in Seattle.');
 } else {
-	alert("Sorry, incorrect. Jesse is and always will be a Seattleite.");
+	alert('Sorry, incorrect. Jesse is and always will be a Seattleite.');
 }
 
 var question2 = prompt('Can Jesse drive a tractor?');
 
-if (question2 === 'Yes') {
-	alert("Wrong! Jesse would be able to CRASH a tractor most likely.");
+if (question2.toUpperCase() === 'NO' || question2.toUpperCase() === 'N') {
+	alert('You are right! Jesse would most likely crash a tractor.');
 } else {
-	alert("You are right! Jesse would most likely crash a tractor.");
+	alert('Wrong! Jesse would be able to CRASH a tractor most likely');
 }
 
 var question3 = prompt('Does Jesse Like soccer?');
 
-if (question3 === 'Yes') {
-	alert("Right on! Yes, Jesse loves soccer.");
+if (question3.toUpperCase() === 'YES' || question3.toUpperCase() === 'Y') {
+	alert('Right on! Yes, Jesse loves soccer.');
 } else {
-	alert("You have never been more wrong in your life. Jesse loves soccer.");
+	alert('You have never been more wrong in your life. Jesse loves soccer.');
 }
 
-console.log('Question 1 response was ' + question1);
+console.log('Exactly what the user entered ' + question1);
+console.log('What I am testing in my "if" condition ' + question1.toUpperCase());
+
 console.log('Question 2 response was ' + question2);
+console.log('What I am testing in my "if" condition ' + question2.toUpperCase());
+
 console.log('Question 3 response was ' + question3);
+console.log('What I am testing in my "if" condition ' + question3.toUpperCase());
 
 alert('Your answer to Question 1 was ' + question1);
 alert('Your answer to Question 2 was ' + question2);
