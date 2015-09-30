56861a28434a2afd4f9b89451f4ac0c046b4b32c
jessethach <jessethach@gmail.com>
2015-09-30 12:04:33 -0700

    this is the initial commit

---
+++ game.js
@@ -0,0 +1,13 @@
+
+
+var question1 = prompt('Is Jesse from Seattle?'); 
+var question2 = prompt('Can Jesse drive a tractor?');
+var question3 = prompt('Does Jesse Like soccer?');
+
+console.log('Question 1 response was ' + question1);
+console.log('Question 2 response was ' + question2);
+console.log('Question 3 response was ' + question3);
+
+alert('Your answer to Question 1 was ' + question1);
+alert('Your answer to Question 2 was ' + question2);
+alert('Your answer to Question 3 was ' + question3);
